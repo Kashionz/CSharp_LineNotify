9fcec27586780023c8dbd2c67d771849313c0ded
Kashionz <kashionz.nicon.s@gmail.com>
2021-07-03 13:59:01 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace CSharp_LineNotify
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 52);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(513, 511);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(392, 25);
+            this.textBox1.TabIndex = 4;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(410, 12);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(114, 25);
+            this.btnSend.TabIndex = 3;
+            this.btnSend.Text = "Send Message";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 576);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnSend);
+            this.Name = "Form1";
+            this.Text = "Line Notify";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_LineNotify
+{
+    public partial class Form1 : Form
+    {
+        string imgPath = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            //VaryQualityLevel();
+            lineNotify_img(textBox1.Text.ToString());
+        }
+
+        //壓縮圖片品質，測試傳輸效率
+        private void VaryQualityLevel()
+        {
+            // Get a bitmap. The using statement ensures objects  
+            // are automatically disposed from memory after use.
+            Bitmap img1 = new Bitmap(pictureBox1.Image);
+
+            //if (System.IO.File.Exists(imgPath))
+            //    System.IO.File.Delete(imgPath);
+
+            ImageCodecInfo jpgEncoder = GetEncoder(ImageFormat.Jpeg);
+
+            // Create an Encoder object based on the GUID  
+            // for the Quality parameter category.  
+            System.Drawing.Imaging.Encoder myEncoder = System.Drawing.Imaging.Encoder.Quality;
+
+            // Create an EncoderParameters object.  
+            // An EncoderParameters object has an array of EncoderParameter  
+            // objects. In this case, there is only one  
+            // EncoderParameter object in the array.  
+            EncoderParameters myEncoderParameters = new EncoderParameters(1);
+            EncoderParameter myEncoderParameter = new EncoderParameter(myEncoder, long.Parse(Path.GetFileNameWithoutExtension(imgPath)));
+            myEncoderParameters.Param[0] = myEncoderParameter;
+            img1.Save(imgPath, jpgEncoder, myEncoderParameters);
+            img1.Dispose();
+            
+        }
+
+        private ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageEncoders();
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+
+        private void lineNotify_img(string msg)
+        {
+            try
+            {
+                var file = imgPath; //圖檔大小必須小於3MB
+                var upfilebytes = File.ReadAllBytes(file);
+                HttpClientHandler handler = new HttpClientHandler();
+                HttpClient Client = new HttpClient(handler);
+                Client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", "gTW2tkn50pz3sCrylA4lpmK5cyCTHpPlrg6ot9PZvbF"); //Token
+                MultipartFormDataContent content = new MultipartFormDataContent();
+                ByteArrayContent baContent = new ByteArrayContent(upfilebytes);
+                content.Add(baContent, "imageFile", Path.GetFileName(imgPath));
+                string url = @"https://notify-api.line.me/api/notify?message=";         //組訊息
+                url = url + msg;
+                var response = Client.PostAsync(url, content).Result;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "PNG(*.PNG); JPG(*.JPG; *.JPEG); gif文件(*.GIF) | *.png; *.jpg; *.jpeg; *.gif";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                imgPath = openFileDialog1.FileName.ToString();
+                FileInfo fs = new FileInfo(imgPath);
+                long size = fs.Length;
+                if (size > 3145728)
+                {
+                    MessageBox.Show("檔案必須小於3MB");
+                    imgPath = "";
+                    pictureBox1.Image = null;
+                    return;
+                }
+                FileStream fss = new FileStream(imgPath, FileMode.Open);
+                Image img1 = Image.FromStream(fss);
+                fss.Close();
+                textBox1.Text = imgPath;
+                pictureBox1.Image = img1;
+            }
+        }
+    }
+}
